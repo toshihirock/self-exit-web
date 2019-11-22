56088fc5a1436e8ac063211abb436a3171d9554e
toshihirock <toshihiro308@gmail.com>
2019-11-22 15:24:31 +0900

    initial check-in

---
+++ app.js
@@ -0,0 +1,25 @@
+var http = require('http');
+
+http.createServer(function (req, res) {
+
+    res.setHeader('Content-Type', 'text/html');
+    console.log(req.url);
+
+    switch (req.url) {
+      case '/about':
+        msg = 'this is about';
+        break;
+      case '/kill':
+        msg = 'this is kill';
+        console.log('die');
+        process.exit(0);
+        break;
+      default:
+        msg = 'this is default';
+        break;
+
+    }
+    res.write(msg);
+    res.end();
+
+}).listen(80); // 127.0.0.1の80番ポートで待機
